639c2077cb91f5f472bf191ed1c7cb449a85e1d8
ShuaChee <el-coyot@el-coyot.com.ua>
2019-03-27 17:02:45 +0200

    add seesion key to response

--- bb_user/api/views/user.py
+++ bb_user/api/views/user.py
@@ -1,7 +1,6 @@
 from django.http import JsonResponse
 from django.views.generic import View
 from utils.api.mixins import APIMixin
-from django.http import HttpResponse
 from django.contrib.auth import get_user_model, authenticate, login, logout
 from django.contrib.auth.hashers import make_password
 
@@ -9,35 +8,27 @@ from django.contrib.auth.hashers import make_password
 class UserRegister(APIMixin, View):
 
     def post(self, request, parameters, *args, **kwargs):
-        password = parameters['password']
-        confirm_password = parameters['confirm_password']
-        email = parameters['email']
-        username = parameters['username']
         user_model = get_user_model()
 
-        if password != confirm_password:
-            return HttpResponse('Check password')
+        if parameters['password'] != parameters['confirm_password']:
+            return {'message': 'Check password'}
 
         try:
             user = user_model.objects.create(
-                username=username,
-                password=make_password(password),
-                email=email
+                username=parameters['username'],
+                password=make_password(parameters['password']),
+                email=parameters['email']
             )
             user.save()
-            return {
-                'message': 'User Registered'
-            }
+            return {'message': 'User Registered'}
         except:
-            return {
-                'message': 'Email or username already taken'
-            }
+            return {'message': 'Email or username already taken'}
 
     def get(self, request, *args, **kwargs):
         # todo: this is for example how to call user model!
         user_model = get_user_model()
         user = user_model.objects.get(id=1)
-        return HttpResponse('Register')
+        return {'message': 'Register'}
 
 
 class UserLogin(APIMixin, View):
@@ -51,13 +42,14 @@ class UserLogin(APIMixin, View):
         else:
             login(request, user)
             return {
-                'message': 'You are logged in'
+                'message': 'You are logged in',
+                'session_key': request.session.session_key
             }
 
 
-class UserLogout(View):
+class UserLogout(APIMixin, View):
     def get(self, request, *args, **kwargs):
         logout(request)
-        return JsonResponse({
+        return {
                 'message': 'Logged out'
-            })
+            }
