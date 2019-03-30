3c6dbf4a2f804156aae8c1764b62771f576e16e5
ShuaChee <el-coyot@el-coyot.com.ua>
2019-03-30 19:15:46 +0200

    APIView class added, JWT-token added

--- bb_user/api/views/user.py
+++ bb_user/api/views/user.py
@@ -1,8 +1,41 @@
-from django.http import JsonResponse
+import json
+import jwt
+from datetime import datetime
+from django.conf import settings
+from django.http import JsonResponse, HttpResponse
 from django.views.generic import View
 from utils.api.mixins import APIMixin
 from django.contrib.auth import get_user_model, authenticate, login, logout
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.hashers import make_password
+from bb_user.models import UserSessions
+
+
+class APIView(View):
+
+    user_model = get_user_model()
+
+    def get_parameters(self, request):
+        parameters = {}
+        try:
+            if request.method in ('POST', 'PUT'):
+                parameters = json.loads(request.body)
+
+            if request.method == 'GET':
+                parameters = request.GET
+
+        except:
+            return JsonResponse({'Message': 'Load Data Error'}, status=500)
+
+        return parameters
+
+    @csrf_exempt
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            result = super(APIView, self).dispatch(request, parameters=self.get_parameters(request), *args, **kwargs)
+        except:
+            return JsonResponse({'Message': 'Something wrong'}, status=500)
+        return result
 
 
 class UserRegister(APIMixin, View):
@@ -31,25 +64,46 @@ class UserRegister(APIMixin, View):
         return {'message': 'Register'}
 
 
-class UserLogin(APIMixin, View):
+class UserLogin(APIView):
     def post(self, request, parameters, *args, **kwargs):
+
+        access_token = request.META.get('Authorization')
+        if access_token:
+            try:
+                session = UserSessions.objects.get(access_token=token)
+            except UserSessions.DoesNotExist:
+                return JsonResponse({'Message': 'Invalid Token'}, status=403)
+
+            if session.expired_at < datetime.now:
+                return JsonResponse({'Message': 'Token Expired'}, status=403)
+            else:
+                user = self.user_model.objects.get(pk=session.user)
+                login(request, user)
+                return JsonResponse({'Message': 'U R Logged In'}, status=200)
+
+
         user = authenticate(username=parameters['username'], password=parameters['password'])
 
         if user is None:
-            return {
+            return JsonResponse({
                 'message': 'Wrong Username or Password'
-            }
+            }, status=403)
         else:
-            login(request, user)
-            return {
+            access_token = 123 #jwt.encode({'user_id': user.id}, settings['SECRET_KEY'], algorithm='HS256')
+            session = UserSessions.objects.create(
+                user=user.id,
+                access_token=access_token
+            )
+            session.save()
+            return JsonResponse({
                 'message': 'You are logged in',
-                'session_key': request.session.session_key
-            }
+                'access_token': access_token
+            }, status=200)
 
 
 class UserLogout(APIMixin, View):
     def get(self, request, *args, **kwargs):
         logout(request)
         return {
-                'message': 'Logged out'
-            }
+            'message': 'Logged out'
+        }
--- bb_user/models.py
+++ bb_user/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -6,8 +7,15 @@ class User(AbstractUser):
     # user_email = models.EmailField(unique=True)
     # user_name = models.CharField(max_length=30, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
     # verified = models.BooleanField(default=False)
     # gravatar = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return '{0} - {1}'.format(self.username, self.email)
+
+
+class UserSessions(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    access_token = models.CharField(max_length=200)
+    expired_at = models.DateTimeField(default=datetime.now() + timedelta(days=30))
